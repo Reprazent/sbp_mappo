dfaf03485a4a5cc0efceea52e3e91c7d510a6518
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2011-01-04 14:12:24 +0100

    Added schedule stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SbpMappo::Application.routes.draw do
+  resources :schedules, :except => [:index, :destroy]
+
   resources :patients
 
   devise_for :users
@@ -52,7 +54,7 @@ SbpMappo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "schedules#show"
 
   # See how all your routes lay out with "rake routes"
 
